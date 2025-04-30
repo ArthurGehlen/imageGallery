6ca611752515a357fb3ea2afb841f7f0fa89010a
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-04-30 20:37:56 -0300

    corrige: substitui BrowserRouter por HashRouter para funcionar no GitHub Pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-// Hooks
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Pages
 import Home from './components/pages/Home';
 import AddCard from './components/pages/AddCard';
 import Support from './components/pages/Support';
 
-// Components
 import Navbar from './components/ui/Navbar';
-import Footer from './components/ui/Footer'
+import Footer from './components/ui/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/' element={<Home />} />
         <Route path="/add_card" element={<AddCard />} />
         <Route path="/support" element={<Support />} />
       </Routes>
